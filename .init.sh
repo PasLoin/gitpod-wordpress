a450cff0d6ef288933be006c759e3705ca9efbd5
PasLoin <raphael0arnould@gmail.com>
2020-05-23 08:30:24 +0200

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -3,3 +3,23 @@ wp plugin delete akismet
 wp plugin activate ${REPO_NAME}
 wp plugin install woocommerce --activate # install WooCommerce
 wp plugin install coopcycle --activate # install Coopcycle plugin @see https://github.com/coopcycle/coopcycle-plugins
+wp theme install storefront --activate 
+wp plugin install Advanced Custom Fields Multilingual --activate
+wp plugin install Akismet Anti-Spam --activate
+wp plugin install Splash Popup for WooCommerce --activate	
+wp plugin install WooCommerce Admin --activate
+wp plugin install WooCommerce Blocks --activate
+wp plugin install WooCommerce Multilingual --activate
+wp plugin install WooCommerce PDF Invoices --activate
+wp plugin install WooCommerce Show Attributes --activate
+wp plugin install WooCommerce Stripe Gateway --activate
+wp plugin install WP All Export --activate
+wp plugin install WP All Import --activate
+wp plugin install WP File Manager --activate
+wp plugin install WP Mail SMTP --activate
+wp plugin install WP-Matomo --activate
+wp plugin install WPML All Import --activate
+wp plugin install WPML Media --activate
+wp plugin install WPML Multilingual CMS --activate
+wp plugin install WPML String Translation --activate
+wp plugin install WPML Translation Management --activate

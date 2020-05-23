a9563fbe349c7ddb7f46980bec46a08e810f0e8f
PasLoin <raphael0arnould@gmail.com>
2020-05-23 11:17:55 +0200

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -1,6 +1,6 @@
 wp plugin delete hello
 wp plugin delete akismet
-wp theme delete Twenty-Twenty
+wp theme delete TwentyTwenty
 wp plugin activate ${REPO_NAME}
 wp theme activate ${REPO_NAME}
 wp plugin install woocommerce --activate # install WooCommerce
@@ -8,23 +8,22 @@ wp plugin install coopcycle --activate # install Coopcycle plugin @see https://g
 wp theme install storefront --activate 
 wp plugin install Advanced-Custom-Fields-Multilingual --activate
 wp plugin install Akismet-Anti-Spam --activate
-# wp plugin install Splash Popup for WooCommerce --activate	
+wp plugin install-Splash-Popup-for-WooCommerce --activate	
 wp plugin install WooCommerce-Admin --activate
 wp plugin install WooCommerce-Blocks --activate
 wp plugin install WooCommerce-Multilingual --activate
-wp plugin install WooCommerce PDF Invoices --activate
-wp plugin install WooCommerce Show Attributes --activate
-wp plugin install WooCommerce Stripe Gateway --activate
-wp plugin install WPAllExport --activate
-wp plugin install WP_All_Export --activate
-wp plugin install WP All Import --activate
-wp plugin install WP File Manager --activate
-wp plugin install WP Mail SMTP by WPForms --activate
+wp plugin install WooCommerce-PDF-Invoices --activate
+wp plugin install WooCommerce-Show-Attributes --activate
+wp plugin install WooCommerce-Stripe-Gateway --activate
+#wp plugin install WPAllExport --activate
+wp plugin install WP-All-Export --activate
+wp plugin install WP-All-Import --activate
+wp plugin install WP-File-Manager --activate
+wp plugin install WP-Mail-SMTP-by-WPForms --activate
 # wp plugin install WP-Matomo (WP-Piwik) --activate
-wp plugin install WPML All Import --activate
-wp plugin install WPML Media --activate
-wp plugin install WPML Multilingual CMS --activate
-wp plugin install WPML String Translation --activate
-wp plugin install WPML Translation Management --activate
-wp plugin install WP Mail SMTP par WPForms --activate
+wp plugin install WPML-All-Import --activate
+wp plugin install WPML-Media --activate
+wp plugin install WPML-Multilingual-CMS --activate
+wp plugin install WPML-String-Translation --activate
+wp plugin install WPML-Translation-Management --activate
 wp plugin install wp-file-manager --activate

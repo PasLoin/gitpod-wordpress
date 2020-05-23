41dec44b047a4913ca1aa2da4642eb39f0171f6e
PasLoin <raphael0arnould@gmail.com>
2020-05-23 08:07:01 +0200

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -1,3 +1,5 @@
 wp plugin delete hello
 wp plugin delete akismet
 wp plugin activate ${REPO_NAME}
+wp plugin install woocommerce --activate # install WooCommerce
+wp plugin install coopcycle --activate # install Coopcycle plugin @see https://github.com/coopcycle/coopcycle-plugins

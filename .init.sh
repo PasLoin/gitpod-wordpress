698affae647dc79408de37623a0a0b3136eb4424
PasLoin <raphael0arnould@gmail.com>
2020-11-01 07:33:17 +0100

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -16,19 +16,19 @@ wp plugin install WooCommerce-Admin --activate
 wp plugin install WooCommerce-Multilingual --activate
 wp plugin install WooCommerce-PDF-Invoices --activate
 # wp plugin install WooCommerce-Show-Attributes --activate
-wp plugin install WooCommerce-Stripe-Gateway --activate
+# wp plugin install WooCommerce-Stripe-Gateway --activate
 wp plugin install WooCommerce Stripe --activate
-#wp plugin install WPAllExport --activate
-wp plugin install WP-All-Export --activate
-wp plugin install WP-All-Import --activate
+# wp plugin install WPAllExport --activate
+# wp plugin install WP-All-Export --activate
+# wp plugin install WP-All-Import --activate
 wp plugin install WP-File-Manager --activate
-wp plugin install WP-Mail-SMTP-by-WPForms --activate
+# wp plugin install WP-Mail-SMTP-by-WPForms --activate
 # wp plugin install WP-Matomo (WP-Piwik) --activate
-wp plugin install WPML-All-Import --activate
-wp plugin install WPML-Media --activate
-wp plugin install WPML-Multilingual-CMS --activate
-wp plugin install WPML-String-Translation --activate
-wp plugin install WPML-Translation-Management --activate
+# wp plugin install WPML-All-Import --activate
+# wp plugin install WPML-Media --activate
+# wp plugin install WPML-Multilingual-CMS --activate
+# wp plugin install WPML-String-Translation --activate
+# wp plugin install WPML-Translation-Management --activate
 wp plugin install wp-file-manager --activate
 wp plugin install Duplicator – WordPress Migration Plugin --activate
 wp theme delete twentytwenty
